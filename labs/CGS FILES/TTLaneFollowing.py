b34d10bd28822a8aa258b3a63be0b323dc458664
Orfeas-Mavros <89010645+Orfeas-Mavros@users.noreply.github.com>
2024-04-08 07:45:05 +0300

    And I know it's bad when we look out, but bad bad people don't live in our house, so...
    A word to all my friends, with their red flags their white knights their black eyes and their blue lights.
    If you're asking yourself: "How do you know?" then that's your answer, that answer is "No."
    You gotta run, gotta run run run run like your head's on fire.
    Run away like your HEAD IS ON FIRE!
    
    Anyway, cue the cellos.
    I finished working on the files.
    As in I don't have any more time.
    I'm not actually finished.
    Cue the trumpets.
    Main is barely functional, the AR_Markers all have identity crises, the racecar wants to commit Sepuku to preserve its honour instead of run our code and then the simulator freezes when you just try to print some stuff.
    Also (and here cue the violins) Lane_Following broke completely and I'm rewriting its logic and so it's not done.
    And also both Lane Following and Line Following need some smart adjustments, but guess what:
    (Let the trumpets blast for a second) I am not smart (Have them blast again, but more dissonant).
    So I need to WORK AAA
    
    Hey I know I was lost but I miss those days.
    I said Ala-lala-la-lala lost but I miss those days!

---
+++ labs/CGS FILES/TTLaneFollowing.py
@@ -0,0 +1,223 @@
+"""
+Copyright MIT and Harvey Mudd College
+MIT License
+Summer 2020
+"""
+
+########################################################################################
+# Imports
+########################################################################################
+
+import sys
+import cv2 as cv
+import numpy as np
+
+sys.path.insert(1, "../../library")
+import racecar_core
+import racecar_utils as rc_utils
+
+########################################################################################
+# Global variables
+########################################################################################
+
+rc = 0
+
+
+targetColor = ""
+minContourArea = 10
+
+croppingMin = ()
+croppingMax = ()
+
+COLOR = ((0, 0, 0), (0, 0, 0))
+PURPLE = ((110,59,50), (165,255,255))
+ORANGE = ((10, 50, 50), (20, 255, 255))
+
+ORANGE_contours = [None, None]
+PURPLE_contours = [None, None]
+
+avg_contour_center = None
+
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+def setUp(raceCar):
+    global rc
+    global croppingMin
+    global croppingMax
+
+    print("Lane Following set up successfully!")
+
+    rc = raceCar
+    croppingMin = (0, 0)
+    croppingMax = (rc.camera.get_height(), rc.camera.get_width())
+
+
+
+def listRemove(multidimensional_list, list_to_remove):
+    for index, sublist in enumerate(multidimensional_list):
+        if np.array_equal(sublist, list_to_remove):
+            del multidimensional_list[index]
+            return multidimensional_list
+
+
+
+def runLaneFollowing(target_color):
+    global speed
+    global angle
+    global targetColor
+
+    targetColor = target_color
+
+
+    update_contour()
+
+
+    angle = 0
+    speed = 0
+
+    if avg_contour_center is not None:
+
+        angle = 2 * avg_contour_center[1] - rc.camera.get_width()
+        angle /= rc.camera.get_width()
+
+        speed = -avg_contour_center[0] + rc.camera.get_height() * 3 // 4
+        speed /= rc.camera.get_height() // 4
+        
+    if speed < -1:
+        speed = -1
+    elif speed > 1:
+        speed = 1
+    if angle < -1:
+        angle = -1
+    elif angle > 1:
+        angle = 1
+
+
+    #rc.drive.set_speed_angle(speed, angle)
+    rc.drive.set_speed_angle(rc.controller.get_trigger(rc.controller.Trigger.RIGHT) - rc.controller.get_trigger(rc.controller.Trigger.LEFT), rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0])
+
+
+
+
+def computeAvg(Contours):
+    
+    global avg_contour_center
+    
+    if len(Contours) == 1:
+                       
+        center = rc_utils.get_contour_center(Contours[0])
+
+        if center[1] < rc.camera.get_width() / 2:
+            avg_contour_center = (center[0], (rc.camera.get_width() + center[1]) // 2)
+
+        else:
+            avg_contour_center = (center[0], center[1] // 2)
+        
+    else:
+        center_1 = rc_utils.get_contour_center(Contours[0])
+        center_2 = rc_utils.get_contour_center(Contours[1])
+        print(center_1[0])
+        print(center_2[0])
+        avg_contour_center = ((center_1[0] + center_2[0]) // 2, (center_1[1] + center_2[1]) // 2)
+
+
+
+def update_contour():
+    
+    global avg_contour_center
+    global ORANGE
+    global PURPLE
+    global ORANGE_contours
+    global PURPLE_contours
+    global minContourArea
+    global contourId
+    global prevContourCenter
+
+
+    ORANGE_contours = [None, None]
+    PURPLE_contours = [None, None]
+
+    image = rc.camera.get_color_image()
+
+
+    if image is None:
+        avg_contour_center = None
+        
+    else:
+        image = rc_utils.crop(image, croppingMin, croppingMax)
+
+        ORANGE_contourlist = list(rc_utils.find_contours(image, ORANGE[0], ORANGE[1]))
+        PURPLE_contourlist = list(rc_utils.find_contours(image, PURPLE[0], PURPLE[1]))
+
+
+        if ORANGE_contourlist is not None:
+            ORANGE_contours[0] = rc_utils.get_largest_contour(ORANGE_contourlist, minContourArea)
+            PURPLE_contourlist = listRemove(PURPLE_contourlist, PURPLE_contours[0])
+
+            if len(ORANGE_contourlist) != 0:
+                ORANGE_contours[1] = rc_utils.get_largest_contour(ORANGE_contourlist, minContourArea)
+
+
+        if PURPLE_contourlist is not None:
+            PURPLE_contours[0] = rc_utils.get_largest_contour(PURPLE_contourlist, minContourArea)
+            PURPLE_contourlist = listRemove(PURPLE_contourlist, PURPLE_contours[0])
+
+            if len(PURPLE_contourlist) != 0:
+                PURPLE_contours[1] = rc_utils.get_largest_contour(PURPLE_contourlist, minContourArea)
+
+
+        if PURPLE_contours is not [None, None] or ORANGE_contours is not [None, None]:
+            
+            print("OR: ", ORANGE_contours)
+            print("PUR:", PURPLE_contours)
+            
+
+            contourNum = 0
+            avg_contour_center = (0, 0)
+
+            if PURPLE_contours != [None, None]:
+                for contour in PURPLE_contours:
+                    contourNum += 1
+                    newCenter = rc_utils.get_contour_center(contour)
+                    avg_contour_center[0] += newCenter[0]
+                    avg_contour_center[1] += newCenter[1]
+
+            if ORANGE_contours != [None, None]:
+                for contour in ORANGE_contours:
+                    contourNum += 1
+                    newCenter = rc_utils.get_contour_center(contour)
+                    avg_contour_center[0] += newCenter[0]
+                    avg_contour_center[1] += newCenter[1]
+
+            avg_contour_center[0] /= contourNum
+            avg_contour_center[1] /= contourNum
+        else:
+            avg_contour_center = None
+
+
+
+
+def start():
+    print(
+        "Time Trial - Lane Following"
+    )
+
+
+def update():
+    runLaneFollowing("Purple")
+
+
+
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc = racecar_core.create_racecar()
+    rc.set_start_update(start, update)
+    rc.go()
---
+++ labs/CGS FILES/main.py
@@ -0,0 +1,245 @@
+# Importations
+
+import sys
+import cv2 as cv
+import numpy as np
+
+sys.path.insert(1, "../../library")
+import racecar_core
+import racecar_utils as rc_utils
+import ar_solver
+from enum import Enum
+
+
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+# Time trial functions from the guys.
+import TTLineFollowing as tt_Line
+import TTLaneFollowing as tt_Lane
+import TTWallFollowing as tt_Wall
+import TTConeSlaloming as tt_Cone
+
+# Here we shall call our time trials:
+def updateLineFollowing():
+	tt_Line.runLineFollowing(color)
+	
+def updateLaneFollowing():
+	tt_Lane.runLaneFollowing(color)
+
+def updateWallFollowing():
+	tt_Wall.update()
+
+def updateSlaloming():
+	tt_Cone.update()
+
+
+
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+
+# ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ #
+# ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ #
+# ~~~ # - #                                   # - # ~~~ #
+					# It'll all turn out ok.
+rc = racecar_core.create_racecar()            # - # ~~~ # Doesn't work? Boo-Hoo.
+			# You're fine   	# You're ok.
+# ~~~ # - #                                   # - # ~~~ #
+# ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ #
+# ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ # - # ~~~ #
+
+
+
+potential_colors = [
+    ("Orange", (10, 50, 50), (20, 255, 255)), # orange
+    ("Blue", (100, 150, 50), (110, 255, 255)), # blue
+    ("Green", (40, 50, 50), (80, 255, 255)),  # green
+    ("Red", (170, 50, 50), (10, 255, 255)), # red
+    ("Purple", (110,59,50), (165,255,255),) # purple
+]
+
+class States(Enum):
+	Idle = 1
+	LineFollowing = 2
+	LaneFollowing = 3
+	Slaloming = 4
+	Walls = 5
+	TurnRight = 6
+	TurnLeft = 7
+
+
+right_counter = 0
+left_counter = 0
+
+color = ()
+state = 1
+
+
+
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+
+def start():
+	global state
+	global color
+
+	state = 1
+	color = potential_colors[1]
+
+	tt_Line.setUp(rc)
+	tt_Lane.setUp(rc)
+	tt_Wall.setUp(rc)
+	tt_Cone.setUp(rc)
+	tt_Line.start()
+	tt_Lane.start()
+	tt_Wall.start()
+	tt_Cone.start()
+
+
+
+def update():
+	global state
+	global color
+	global left_counter
+	global right_counter
+
+	# We can run it here Yioupi!
+	main()
+	# And it doesn't create like 50 RaceCars!!!
+	# Extra Yioupi!
+
+	if left_counter > 0:
+		rc.drive.set_speed_angle(0.8, -0.8)
+		left_counter -= rc.get_delta_time()
+		return
+	
+	elif right_counter > 0:
+		rc.drive.set_speed_angle(0.8, 0.8)
+		right_counter -= rc.get_delta_time()
+		return
+
+
+	if state == States.LineFollowing:
+		updateLineFollowing()
+	
+	elif state == States.LaneFollowing:
+		updateLaneFollowing()
+	
+	elif state == States.Slaloming:
+		updateSlaloming()
+	
+	else:
+		updateWallFollowing()
+	
+
+	#rc.drive.set_speed_angle(rc.controller.get_trigger(rc.controller.Trigger.RIGHT) - rc.controller.get_trigger(rc.controller.Trigger.LEFT), rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0])
+
+
+
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+
+def main():
+	global state
+	global color
+	global left_counter
+	global right_counter
+
+	#ar_markers = ar_solver.get_markers_info(rc)
+	#print(ar_markers)
+
+	"""
+	if len(ar_markers) != 0:
+		
+		if ar_markers[0] == "Follow the RED Line":
+			color = potential_colors[3]
+			state = States.LineFollowing
+		elif ar_markers[0] == "Follow the GREEN Line":
+			color = potential_colors[2]
+			state = States.LineFollowing
+		elif ar_markers[0] == "Follow the BLUE Line":
+			color = potential_colors[1]
+			state = States.LineFollowing
+
+		elif ar_markers[0] == "ORANGE Lane Following":
+			color = potential_colors[0]
+			state = States.LaneFollowing
+		elif ar_markers[0] == "PURPLE Lane Following":
+			color = potential_colors[4]
+			state = States.LaneFollowing
+		
+		elif ar_markers[0] == "Slalom":
+			state = States.Slaloming
+		
+		elif ar_markers[0] == "Turn Left":
+			left_counter = 1
+		elif ar_markers[0] == "Turn Right":
+			right_counter = 1
+		
+		else:
+			state = States.Walls
+	"""	
+	# """
+	if rc.controller.was_pressed(rc.controller.Button.A):
+		color = potential_colors[3][0]
+		state = States.LineFollowing
+	elif rc.controller.was_pressed(rc.controller.Button.B):
+		color = potential_colors[2][0]
+		state = States.LineFollowing
+	elif rc.controller.was_pressed(rc.controller.Button.X):
+		color = potential_colors[1][0]
+		state = States.LineFollowing
+
+	elif rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0] == -1:
+		color = potential_colors[0][0]
+		state = States.LaneFollowing
+	elif rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0] == 1:
+		color = potential_colors[4][0]
+		state = States.LaneFollowing
+		
+	elif rc.controller.was_pressed(rc.controller.Button.X):
+		state = States.Slaloming
+		
+	elif rc.controller.get_trigger(rc.controller.Trigger.LEFT) == 1:
+		left_counter = 1
+	elif rc.controller.get_trigger(rc.controller.Trigger.RIGHT) == 1:
+		right_counter = 1
+		
+	elif rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0] == -1 and rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0] == 1:
+		state = States.Walls
+	# """
+"""
+Les Messages
+"Follow the RED Line"
+"Follow the BLUE Line"
+"Follow the GREEN Line"
+"PURPLE Lane Following"
+"ORANGE Lane Following"
+"Slalom"
+"Turn Left"
+"Turn Right"
+"""
+
+
+
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+# -- # -- # -- # -- # -- # -- # -- # -- # -- #
+
+
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update, None)
+    rc.go()
+	
